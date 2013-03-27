02910857c4b078fddc78472a5f61fbe5d7cddd3b
Phillip Lackey <phillip.lackey@athlonsports.com>
2013-03-27 08:52:36 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapProto::Application.config.secret_token = '23d6b08a163aa91b17cfe344e1c807a004ea0ac2aac6fd27c6fa3d43abcf1edfd0e9287529c7855854c613bb11e28f85e00d7d5a82e992063f51aafd691b2eed'
